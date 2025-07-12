c50d93092cb5fe77f02cbe11d36c502789f1ee5b
Eeshu Narayan <eeshunarayan15@gmail.com>
2025-07-12 22:43:30 +0530

    used string buffer to achive synchronization to avoid race around condition

--- src/com/raceconditioninstring/Account.java
+++ src/com/raceconditioninstring/Account.java
@@ -1,14 +1,19 @@
 package com.raceconditioninstring;
 
 public class Account {
-    private  String s="pqrs";
-    public  synchronized void deduct(String a,String cardName){
-           this.s+=a;
-        System.out.println("Debit card Successfully  "+cardName);
+//    private  String s="pqrs";//immutable
+    //StirngBuffer and String Builder muttable String
+//    StringBuffer synchronised
+    private  StringBuffer s=new StringBuffer("xyz");
+    //removed synchronized keyword as we are using stringBuffer
+    public   void deduct(String a,String cardName){
+//           this.s+=a;
+        this.s.append(a);
+//        System.out.println("Debit card Successfully  "+cardName);
     }
     public void get(){
         int count=0;
-        char[] charArray = s.toCharArray();
+        char[] charArray = s.toString().toCharArray();
         for(char c : charArray){
             if(c=='a'){
                 count++;
