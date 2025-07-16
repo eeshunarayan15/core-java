1168aa93e33789540df45985cc28d17eb29f3ef1
Eeshu Narayan <eeshunarayan15@gmail.com>
2025-07-17 01:42:47 +0530

    Primitive to Object transformation

--- src/com/collectionframework/primitiveToObject/Launch.java
+++ src/com/collectionframework/primitiveToObject/Launch.java
@@ -4,8 +4,13 @@ public class Launch {
     public static void main(String[] args) {
         int a=10;
         Integer i =new Integer(a);//manual boxing
-        String string = i.toString();
+        String string = i.toString();//manual inboxing
         System.out.println(string);
+        int c=20;
+        Integer d=c; //auto boxing
+        int k=d;//auto unboxing
+        System.out.println(d);
+
 
     }
 }
