f03c273d3bbfc67028195909b99052c3917b7468
Eeshu Narayan <eeshunarayan15@gmail.com>
2025-07-20 11:05:37 +0530

    implemented generic stack

--- src/com/collectionframework/stack/GenericStack.java
+++ src/com/collectionframework/stack/GenericStack.java
@@ -1,4 +1,17 @@
 package com.collectionframework.stack;
 
+import java.util.Stack;
+
 public class GenericStack {
+    public static void main(String[] args) {
+        Stack<String> strings = new Stack<>();
+        Stack<Integer> integers = new Stack<>();
+        strings.add("hello");
+        strings.add("hello");
+        System.out.println(strings);
+
+        integers.add(54);
+        integers.add(60);
+        System.out.println(integers);
+    }
 }
